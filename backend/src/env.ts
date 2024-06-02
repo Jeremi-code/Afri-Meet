808381f281ffd0024975d5a5e40ded1958f20e40
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-02 14:14:36 -0700

    feat : add zod schema validation to environment variable

---
+++ backend/src/env.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+import { fromError } from "zod-validation-error";
+import dotenv from "dotenv";
+import path from "path";
+
+// Specify the relative path to the .env file
+dotenv.config({ path: path.resolve(__dirname, '../env') });
+
+console.log("Loaded environment variables:", process.env);
+
+const envSchema = z.object({
+  EXPRESS_PORT: z
+    .string()
+    .transform((val) => parseInt(val, 10))
+    .refine((val) => val > 0 && val < 65536 && !isNaN(val), {
+      message: "Invalid port",
+    }),
+  POSTGRES_HOST: z.string(),
+  POSTGRES_PORT: z.coerce.number(),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  HASURA_GRAPHQL_ADMIN_SECRET: z.string(),
+  HASURA_GRAPHQL_DATABASE_URL: z.string().url(),
+  HASURA_GRAPHQL_SERVER_PORT: z.coerce.number(),
+});
+
+const parsedEnv = envSchema.safeParse(process.env);
+if (!parsedEnv.success) {
+  throw new Error(fromError(parsedEnv.error).message);
+}
+
+export const env = parsedEnv.data;
