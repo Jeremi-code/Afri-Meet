8df4b18ed2ef4439faa1681d2024cfcbf49d0965
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-05 05:24:46 -0700

    feat : change the document property to contain .graphql and .gql extensions

--- backend/codegen.ts
+++ backend/codegen.ts
@@ -2,8 +2,8 @@ import { CodegenConfig } from "@graphql-codegen/cli";
 import { TypeScriptPluginConfig } from "@graphql-codegen/typescript";
 const config: CodegenConfig = {
   overwrite : true,
-  schema: "./gql/schema.gql",
-  documents: ["graphql/**/*.graphql"],
+  schema: "./gqlGen/schema.gql",
+  documents: ["graphql/**/*.{graphql,gql}"],
   generates: {
     ".gql/types.ts": {
       plugins: ["typescript", "typescript-operations", "type-document-node"],
