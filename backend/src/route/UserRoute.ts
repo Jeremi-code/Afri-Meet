61c6aba97cef17651ed2d2c22a49ed18ce629e8b
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-06 11:59:04 -0700

    feat : use router to handle specific endpoint requests

--- backend/src/route/UserRoute.ts
+++ backend/src/route/UserRoute.ts
@@ -1,5 +1,7 @@
 import express, { Router } from 'express'
-import registerUser from '../controller/UserController.'
+import registerUser from '../controller/UserController'
 const router : Router = express.Router()
 
 router.post('/signup',registerUser)
+
+export default router
