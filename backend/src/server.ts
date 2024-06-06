e4f5b35a8ac8e4a3d20387111326db48173081f6
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-06 11:57:16 -0700

    feat : make the server listen to a port and define route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import router from "./route/UserRoute";
 import { env } from "./env";
+import cors from 'cors' 
 
+// console.log(process.env.EXPRESS_PORT)
 const app = express();
 
 const PORT = env.EXPRESS_PORT;
+
+app.use(express.json());
+app.use(router);
+app.use(cors())
+app.get('/',(req,res) => {
+  res.send('Hello World')
+}
+)
+console.log('za')
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
