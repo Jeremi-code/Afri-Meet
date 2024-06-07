c5e55e72109925982add6a218a022c13747b0aaf
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-07 01:29:33 -0700

    refactor : apply modularity

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import { z } from "zod";
 import { fromZodError } from "zod-validation-error";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import { InsertUserDocument } from "../../gqlGen/types";
+import genToken from "../utils/genToken";
 import client from "../utils/client";
 
 const userSchema = z.object({
@@ -13,20 +13,18 @@ const userSchema = z.object({
   password: z.string().min(6),
 });
 
-const SECRET_KEY = z.string()
 
 const registerUser = async (req: Request, res: Response) => {
   try {
     const validateData = userSchema.safeParse(req.body.input);
-    const valSecretKey = SECRET_KEY.parse(process.env.JWT_SECRET)
+    
     if (!validateData.success) {
       throw fromZodError(validateData.error);
     }
     const { first_name, last_name, email, password } = validateData.data;
     const hashedPassword = bcrypt.hashSync(password, 10);
-    jwt.sign({ email },valSecretKey, { expiresIn: "1h" });
 
-    await client.mutate({
+    const result = await client.mutate({
       mutation: InsertUserDocument,
       variables: {
         obj: {
@@ -37,6 +35,9 @@ const registerUser = async (req: Request, res: Response) => {
         },
       },
     });
+    const user_id = result.data?.user?.user_id
+    const token = genToken(user_id as Number)
+    return res.json({user_id,token})
   } catch (error: any) {
     console.log(`Error occured : ${error}`);
   }
