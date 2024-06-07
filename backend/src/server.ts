8fcba8fe11787249c0432c8944a091e1481600d4
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-07 00:30:07 -0700

    refactor : format the code and remove debugging codes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,20 @@
 import express from "express";
 import router from "./route/UserRoute";
 import { env } from "./env";
-import cors from 'cors' 
+import cors from "cors";
+import bodyParser from "body-parser";
 
-// console.log(process.env.EXPRESS_PORT)
 const app = express();
 
 const PORT = env.EXPRESS_PORT;
 
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(router);
-app.use(cors())
-app.get('/',(req,res) => {
-  res.send('Hello World')
-}
-)
-console.log('za')
-
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
