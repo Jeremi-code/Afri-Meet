4e2de09028667c93bfe416183ce7eb5c076843e5
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-07 06:04:04 -0700

    feat : add login endpoint that handle signing in

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -2,9 +2,13 @@ import { Request, Response } from "express";
 import { z } from "zod";
 import { fromZodError } from "zod-validation-error";
 import bcrypt from "bcryptjs";
-import { InsertUserDocument } from "../../gqlGen/types";
+import {
+  FindUserByEmailDocument,
+  InsertUserDocument,
+} from "../../gqlGen/types";
 import genToken from "../utils/genToken";
 import client from "../utils/client";
+import { error } from "console";
 
 const userSchema = z.object({
   first_name: z.string().min(2),
@@ -13,11 +17,10 @@ const userSchema = z.object({
   password: z.string().min(6),
 });
 
-
 const registerUser = async (req: Request, res: Response) => {
   try {
     const validateData = userSchema.safeParse(req.body.input);
-    
+
     if (!validateData.success) {
       throw fromZodError(validateData.error);
     }
@@ -35,12 +38,44 @@ const registerUser = async (req: Request, res: Response) => {
         },
       },
     });
-    const user_id = result.data?.user?.user_id
-    const token = genToken(user_id as Number)
-    return res.json({user_id,token})
+    const user_id = result.data?.user?.user_id;
+    const token = genToken(user_id as Number);
+    return res.json({ user_id, token });
   } catch (error: any) {
     console.log(`Error occured : ${error}`);
   }
 };
 
-export default registerUser;
+const authUser = async (req: Request, res: Response) => {
+  try {
+    const loginSchema = z.object({
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+    console.log(req.body.input);
+    const validateData = loginSchema.safeParse(req.body.input);
+    if (!validateData.success) {
+      throw fromZodError(validateData.error);
+    }
+    const { email, password } = validateData.data;
+    const result = await client.query({
+      query: FindUserByEmailDocument,
+      variables: { email },
+    });
+    const user_id = result.data?.user[0].user_id;
+    if (!user_id) {
+      throw error("email does not exist");
+    }
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const token = genToken(user_id);
+    if (hashedPassword !== password) {
+      throw new Error("password does not match");
+    }
+    console.log(result.data?.user[0]?.user_id);
+    return res.json({ user_id, token });
+  } catch (error) {
+    return res.status(401).json({error});
+  }
+};
+
+export { registerUser, authUser };
