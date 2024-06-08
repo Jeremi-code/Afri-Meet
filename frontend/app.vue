7dea8e50cee39d3a0f1b96b6c94b7bfe6d3ee915
Ermias S. Deresse <ermiassintayehu13@gmail.com>
2024-06-08 06:51:44 -0700

    export hasura metadata and migrations

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class = "text-green">
+      <NuxtPage />
   </div>
 </template>
+
+<script lang="ts" setup>
+const { clients, getToken, onLogin, onLogout } = useApollo()
+const query = gql`
+query getShips($limit: Int!) {
+  ships(limit: $limit) {
+    id
+  }
+}`
+
+const { data } = await useAsyncQuery(query, { limit: 2 })
+
+if (data.value?.ships) {
+  // log response
+  console.log(data.value.ships)
+}
+
+
+</script>
