159a939a35c49956add4fcc095058142ad70aa21
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-11 11:50:26 +0300

    feat : define layout with NuxtLayout wrapper

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,24 +1,8 @@
 <template>
-  <div class = "text-green">
-      <NuxtPage />
-  </div>
+  <NuxtLayout>
+    <Layout/>
+  </NuxtLayout>
 </template>
-
-<script lang="ts" setup>
-const { clients, getToken, onLogin, onLogout } = useApollo()
-const query = gql`
-query getShips($limit: Int!) {
-  ships(limit: $limit) {
-    id
-  }
-}`
-
-const { data } = await useAsyncQuery(query, { limit: 2 })
-
-if (data.value?.ships) {
-  // log response
-  console.log(data.value.ships)
-}
-
-
+<script setup>
+  import Layout from "./layouts/Layout.vue"
 </script>
