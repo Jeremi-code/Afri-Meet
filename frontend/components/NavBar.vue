e21a0241583c397ea71a40f58e81917bde34468e
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-11 11:52:08 +0300

    define custom navigation bar

---
+++ frontend/components/NavBar.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="sticky top-0 w-screen  bg-white h-16 z-50 border border-">
+        <UContainer class="container mx-auto px-4 py-2 flex justify-between items-center h-full">
+            <ULink class="text-lg font-bold text-gray-600 hover:text-gray transition-colors duration-300" to="/">
+                Afrimeet
+            </ULink>
+            <ul class="hidden md:flex space-x-4">
+                <li class="flex items-center">
+                    <BaseNavLink label="Meetings" to="/meetings" />
+                </li>
+                <li class="flex items-center">
+                    <BaseNavLink label="Rooms" to="/rooms" />
+                </li>
+                <li class="flex items-center">
+                    <BaseNavLink label="About us" to="/about" />
+                </li>
+                <li class="flex items-center">
+                    <BaseNavLink label="FAQ" to="/faq" />
+                </li>
+                <div class="flex flex-row px-2">
+                    <li>
+                        <UButton @click="register" class="border-50 " to="/register">
+                            Register
+                        </UButton>
+                    </li>
+                    <li class="pl-3 text-lg">
+                        <UButton @click="login" class=" text-sm hover:text-gray-900 transition-colors duration-300"
+                            to="/login" variant="link">
+                            Login
+                        </UButton>
+                    </li>
+
+                </div>
+            </ul>
+        </UContainer>
+    </nav>
+</template>
+
+<script setup lang="ts">
+const activeLink = ref<string>('/')
+
+const setActiveLink = (route: string) => {
+    activeLink.value = route
+}
+
+</script>
