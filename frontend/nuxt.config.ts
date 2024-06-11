0a5f55c64deb87c84e321a9b072c4dab3c9d39ed
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-11 11:51:48 +0300

    feat : add nuxt ui inside modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:['@nuxtjs/tailwindcss','@nuxtjs/apollo'],
+  modules:['@nuxtjs/tailwindcss','@nuxtjs/apollo',"@nuxt/ui"],
   apollo: {
     clients: {
       default: {
