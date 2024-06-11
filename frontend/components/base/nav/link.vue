78953b4841214bd58dc984cfc7f62b2099c9522c
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-11 11:53:16 +0300

    refactor : by endorsing nuxt folder strucutre pros , apply modularity

---
+++ frontend/components/base/nav/link.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+    to: string
+    label: string
+}>()
+</script>
+<template>
+    <ULink active-class="font-bold border-b-gray-600 border-b-2" class="text-gray-600 hover:text-gray-900 transition-colors duration-300 text-sm"
+        :to>
+        {{ label }}
+    </ULink>
+</template>
