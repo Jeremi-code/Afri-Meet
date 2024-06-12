5eff1d2150b0ab268289ffdd12da2d21c81f16c8
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-12 11:39:58 +0300

    fix : delete nuxtjs/tailwind from modules

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:['@nuxtjs/tailwindcss','@nuxtjs/apollo',"@nuxt/ui"],
+  modules:['@nuxtjs/apollo',"@nuxt/ui"],
   apollo: {
     clients: {
       default: {
