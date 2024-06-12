da8029fb33ee19938c9d7614f40181afc278b433
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-12 20:04:57 +0300

    chore : add codegen configuration

---
+++ frontend/codegen.ts
@@ -0,0 +1,15 @@
+import type { CodegenConfig } from "@graphql-codegen/cli";
+import type { TypeScriptPluginConfig } from "@graphql-codegen/typescript";
+import { getNullableType } from "graphql";
+const config: CodegenConfig = {
+  overwrite : true,
+  schema: "./gqlGen/schema.gql",
+  documents: ["graphql/**/*.{graphql,gql}"],
+  generates: {
+    "./gqlGen/types.ts": {
+      plugins: ["typescript", "typescript-operations","typed-document-node"],
+      config : {} satisfies TypeScriptPluginConfig  
+    },
+  },
+};
+export default config;
