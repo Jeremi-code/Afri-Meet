abbfa2f19f3c7d6956933bd150bf178878e7728e
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-13 20:17:19 +0300

    config : add configuration of nuxt/apollo to make nuxt/apollo work

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import "@nuxt3/graphql-codegen-module";
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules:['@nuxtjs/apollo',"@nuxt/ui"],
+  modules: ["@nuxtjs/apollo", "@nuxt/ui", "@nuxt3/graphql-codegen-module"],
   apollo: {
     clients: {
       default: {
-        httpEndpoint: 'http://localhost:8080/v1/graphql'
-      }
+        httpEndpoint: "http://localhost:8080/v1/graphql",
+      },
     },
   },
-
-})
+});
