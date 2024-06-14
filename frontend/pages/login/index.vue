0bec1c82abf56804a6c46787f1be99830c73246b
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-14 22:13:42 +0300

    feat : set-cookie and validate for error

--- frontend/pages/login/index.vue
+++ frontend/pages/login/index.vue
@@ -7,27 +7,34 @@
                 <form @submit.prevent="submitForm">
                     <div class="mb-4">
                         <label for="email" class="block text-gray-700 font-bold">Email:</label>
-                        <input type="text" id="email" @input="closeError()" v-model="form.email" required 
+                        <input type="text" id="email" @input="closeError()" v-model="form.email" required
                             class="mt-1 block w-full rounded-md border border-gray-300 py-[5px] px-3 bg-white shadow-sm" />
-                            <div v-if="formErrorVisible && errors.email " class="text-red-500 text-sm mt-1">{{ errors.email}}</div>
-                        </div>
+                        <div v-if="formErrorVisible && errors.email" class="text-red-500 text-sm mt-1">{{
+                            errors.email }}</div>
+                    </div>
                     <div class="mb-7">
                         <label for="password" class="block text-gray-700 font-bold">Password:</label>
-                        <div class = "relative">
-                            <input :type="passwordVisible ? 'text' : 'password'" id="password" @input="closeError" v-model="form.password"
-                                required
+                        <div class="relative">
+                            <input :type="passwordVisible ? 'text' : 'password'" id="password" @input="closeError"
+                                v-model="form.password" required
                                 class="mt-1 block w-full rounded-md border border-gray-300 py-[5px] px-3 shadow-sm bg-white" />
                             <div @click="togglePasswordVisibility"
                                 class="absolute right-3 top-1/2 transform -translate-y-1/2 cursor-pointer text-gray-700">
-                                <UIcon name="fluent:eye-16-regular" v-if="!passwordVisible"/>
+                                <UIcon name="fluent:eye-16-regular" v-if="!passwordVisible" />
                                 <UIcon name="fluent:eye-off-24-regular" v-if="passwordVisible" />
                             </div>
-                            </div>
-                        <div v-if="formErrorVisible && errors.password" class="text-red-500 text-sm mt-1">{{ errors.password }}</div>
+                        </div>
+                        <div v-if="formErrorVisible && errors.password" class="text-red-500 text-sm mt-1">{{
+                            errors.password }}</div>
                     </div>
-                    <button type="submit"
+                    <button type="submit" :disabled="loading"
                         class="w-full bg-[#84CC16] text-white py-[5px] px-4 rounded-md transition-colors hover:opacity-75 mb-5">
-                        Sign Up
+                        <template v-if="loading">
+                            <div class="spinner mr-2"></div> Loading...
+                        </template>
+                        <template v-else>
+                            Sign In
+                        </template>
                     </button>
                     <ULink to="/register" class="mb-5 text-blue-500 underline flex items-center justify-center">
                         Create new account
@@ -44,8 +51,8 @@ import z from 'zod';
 import { useQuery } from '@vue/apollo-composable';
 import { LoginDocument } from '~/gqlGen/types';
 const signinForm = z.object({
-    email : z.string().email(),
-    password : z.string().min(6)
+    email: z.string().email(),
+    password: z.string().min(6)
 })
 
 interface Form {
@@ -67,47 +74,107 @@ const passwordVisible = ref(false);
 const togglePasswordVisibility = () => {
     passwordVisible.value = !passwordVisible.value;
 };
-const { result, loading,error,refetch } = useQuery(LoginDocument,() => ({
-    input: {
-      email: form.value.email,
-      password: form.value.password,
-    }
-  }),
-  { enabled: false } 
-);
-
+const { loading, error, mutate } = useMutation(LoginDocument)
 const router = useRouter()
+const authToken = useCookie('auth-token')
 
 const submitForm = async () => {
-    console.log('Initial', form.value);
-    
-    const result = signinForm.safeParse(form.value)
-    if (!result.success) {
-        errors.value = {}
-        result.error.errors.forEach((err) => {
-            errors.value[err.path[0]] = err.message;
-            formErrorVisible.value = true
-        });
-        return;
+    try {
+        const result = signinForm.safeParse(form.value)
+        if (!result.success) {
+            errors.value = {}
+            result.error.errors.forEach((err) => {
+                errors.value[err.path[0]] = err.message;
+                formErrorVisible.value = true
+            });
+            return;
+        }
+        const response = await mutate({
+            input: {
+                email: result.data.email,
+                password: result.data.password
+            }
+        })
+        if (response?.data?.login?.token) {
+            if (authToken.value) {
+                console.log(authToken.value)
+                authToken.value = null
+                console.log(authToken.value)
+            }
+            authToken.value = response?.data?.login?.token
+            form.value.email = ''
+            form.value.password = ''
+            router.push('/meetings')
+        }
+        else if (response?.errors && response.errors.length > 0) {
+            const emailErr = response.errors[0]?.message
+            throw new Error(emailErr)
+        }
+        console.log('Form submitted:', response);
+    } catch (error: any) {
+        errors.value.email = error.message
+        formErrorVisible.value = true
     }
-    await refetch()
 
-    console.log('Form submitted:', form.value);
-};
-watchEffect(() => {
-  if (result.value) {
-    const token = result.value.findUser?.token;
-    if (token) {
-      const tokenCookie = useCookie('token');
-      tokenCookie.value = token;
-      router.push('/meetings')
-    }
-  }
-  
-  if (error.value) {
-    console.error(error.value);
-    formErrorVisible.value = true;
-    errors.value.general = 'An error occurred. Please try again.';
-  }
-});
+//     const result = signinForm.safeParse(form.value)
+//     if (!result.success) {
+//         errors.value = {}
+//         result.error.errors.forEach((err) => {
+//             errors.value[err.path[0]] = err.message;
+//             formErrorVisible.value = true
+//         });
+//         return;
+//     }
+//     const response = await mutate({
+//         input: {
+//             email: result.data.email,
+//             password: result.data.password
+//         }
+//     })
+//     if (response?.data?.login?.token) {
+//         authToken.value = response?.data?.signup?.token
+//     }
+//     else if (response?.errors && response.errors.length > 0) {
+//         const emailErr = response.errors[0]?.message
+//         throw new Error(emailErr)
+//     }
+//     console.log('Form submitted:', response);
+
+// watchEffect(() => {
+// if (result.value) {
+//     console.log(result.value)
+//     const token = result.value.findUser?.token;
+//     if (token) {
+//         const authToken = useCookie('auth-token');
+//         if (authToken.value) {
+//             authToken.value = null
+//         }
+//         authToken.value = token;
+//         router.push('/meetings')
+//     }
+// }
+
+// if (error.value) {
+//     console.error(error.value);
+//     formErrorVisible.value = true;
+//     errors.value.general = 'An error occurred. Please try again.';
+// }
+// });
+}
 </script>
+<style scoped>
+.spinner {
+    border: 2px solid rgba(255, 255, 255, 0.3);
+    border-left-color: white;
+    border-radius: 50%;
+    width: 16px;
+    height: 16px;
+    animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+    to {
+        transform: rotate(360deg);
+    }
+}
+</style>
