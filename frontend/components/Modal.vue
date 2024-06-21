f4accbfd6ee91157c2a1d550572f5e76390d4272
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-22 00:28:26 +0300

    feat : display first name,last name and email of the user in uselectmenu

--- frontend/components/Modal.vue
+++ frontend/components/Modal.vue
@@ -1,13 +1,16 @@
 <template>
-  <UButton class="text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3 flex-1" label="Reserve" @click="isOpen = true" />
-  <UModal v-model="isOpen" >
+  <UButton
+    class="text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3 flex-1"
+    label="Reserve" @click="isOpen = true" />
+  <UModal v-model="isOpen">
     <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
       <template #header>
         <div class="flex items-center justify-between">
           <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
             Reserve {{ room.room_name }}
           </h3>
-          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1" @click="isOpen = false" />
+          <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+            @click="isOpen = false" />
         </div>
       </template>
       <form class="space-y-4">
@@ -33,9 +36,9 @@
         </div>
         <div class="relative mt-1">
           <label for="participants">Participants</label>
-          <USelectMenu v-model="selectedParticipants" multiple :options="participants" id="participants">
+          <USelectMenu v-if="participants !== null" v-model="selectedParticipants" multiple :options="formattedParticipants" id="participants">
             <template #leading>
-              <UIcon v-if="selectedParticipants.icon" :name="(selectedParticipants.icon as string)" class="w-5 h-5" />
+              <!-- {{ selectedParticipants.map(p => `${p.first_name} ${p.last_name} (${p.email})`).join(', ') }} -->
             </template>
             <template #empty>
               None Selected
@@ -52,56 +55,40 @@
 
 <script lang="ts" setup>
 import type { ResultOf } from '@graphql-typed-document-node/core';
-import type { GetRoomByIdDocument } from '~/gqlGen/types';
-import { ref } from 'vue';
+import { GetUsersDocument, type GetRoomByIdDocument } from '~/gqlGen/types';
+import { ref, computed, watchEffect, toRaw } from 'vue';
 
-type Room = ResultOf<typeof GetRoomByIdDocument>['room'][number]
+type Room = ResultOf<typeof GetRoomByIdDocument>['room'][number];
 const props = defineProps<{
-  room : Room
+  room: Room
 }>();
 
 const isOpen = ref(false);
 
-interface Participant {
-  id: string;
-  label: string;
-  href?: string;
-  target?: string;
-  avatar?: { src: string };
-  icon?: string;
+interface Participants {
+  first_name: string;
+  last_name: string;
+  email: string;
 }
 
-const participants: Participant[] = [
-  {
-    id: 'benjamincanac',
-    label: 'benjamincanac',
-    href: 'https://github.com/benjamincanac',
-    target: '_blank',
-    avatar: { src: 'https://avatars.githubusercontent.com/u/739984?v=4' }
-  },
-  {
-    id: 'Atinux',
-    label: 'Atinux',
-    href: 'https://github.com/Atinux',
-    target: '_blank',
-    avatar: { src: 'https://avatars.githubusercontent.com/u/904724?v=4' }
-  },
-  {
-    id: 'smarroufin',
-    label: 'smarroufin',
-    href: 'https://github.com/smarroufin',
-    target: '_blank',
-    avatar: { src: 'https://avatars.githubusercontent.com/u/7547335?v=4' }
-  },
-  {
-    id: 'nobody',
-    label: 'Nobody',
-    icon: 'i-heroicons-user-circle'
+const participants = ref<Participants[] | null>(null);
+const { data } = useAsyncQuery(GetUsersDocument);
+const selectedParticipants = ref<Participants[]>([]);
+
+watchEffect(() => {
+  if (data.value) {
+    participants.value = data.value.users;
   }
-];
+});
 
-const selectedRoom = ref(props.room.room_name)
+const selectedRoom = ref(props.room.room_name);
 
-const selectedParticipants = ref<Participant>(participants[0]);
 const rooms = ['Conference Room A', 'Boardroom', 'Executive Suite', 'Huddle Room'];
+
+const formattedParticipants = computed(() =>
+  participants.value ? participants.value.map(p => ({
+    label: `${p.first_name} ${p.last_name} (${p.email})`,
+    ...p
+  })) : []
+);
 </script>
