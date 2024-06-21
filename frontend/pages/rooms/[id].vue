3ca9a94a003a66f8fa8c0a865e3d107a41928024
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-21 21:03:14 +0300

    fix : remove view details button inside id.vue

--- frontend/pages/rooms/[id].vue
+++ frontend/pages/rooms/[id].vue
@@ -39,10 +39,6 @@
               class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 text-primary-foreground h-11 rounded-md px-8 flex-1 bg-lime-500 hover:bg-lime-600 focus-visible:ring-lime-700">
               Reserve
             </button>
-            <button
-              class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-11 rounded-md px-8 flex-1">
-              View Details
-            </button>
           </div>
         </div>
       </div>
