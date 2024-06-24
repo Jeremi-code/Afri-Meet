8e14b10074b368170e3ca8f673017ecfa8ddff4e
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-24 13:47:10 +0300

    fix : solve time and date validatioin error

--- frontend/components/Modal.vue
+++ frontend/components/Modal.vue
@@ -2,9 +2,7 @@
   <div>
     <UButton
       class="text-white inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 bg-primary text-primary-foreground hover:bg-primary/90 h-9 rounded-md px-3 flex-1"
-      label="Reserve"
-      @click="isOpen = true"
-    />
+      label="Reserve" @click="isOpen = true" />
     <UModal v-model="isOpen">
       <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
         <template #header>
@@ -12,33 +10,18 @@
             <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
               Reserve {{ room.room_name }}
             </h3>
-            <UButton
-              color="gray"
-              variant="ghost"
-              icon="i-heroicons-x-mark-20-solid"
-              class="-my-1"
-              @click="isOpen = false"
-            />
+            <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
+              @click="isOpen = false" />
           </div>
         </template>
 
-        <UForm
-          class="space-y-4"
-          :schema="meetingForm"
-          :state="meeting"
-          @submit.prevent="onSubmit"
-        >
+        <UForm class="space-y-4" :schema="meetingForm" :state="meeting" @submit.prevent="onSubmit">
           <UFormGroup label="Meeting Title" name="title">
             <UInput v-model="meeting.title" id="title" />
           </UFormGroup>
 
           <UFormGroup label="Room" name="room">
-            <UInputMenu
-              v-model="meeting.room"
-              :options="rooms"
-              placeholder="Select a room"
-              id="room"
-            />
+            <UInputMenu v-model="meeting.room" :options="rooms" placeholder="Select a room" id="room" />
           </UFormGroup>
 
           <UFormGroup label="Date" name="date">
@@ -54,18 +37,10 @@
           </UFormGroup>
 
           <UFormGroup label="Participants" name="participants">
-            <USelectMenu
-              v-if="participants !== null"
-              v-model="meeting.formattedParticipants"
-              multiple
-              :options="formattedParticipants"
-              id="participants"
-            >
+            <USelectMenu v-if="participants !== null" v-model="meeting.formattedParticipants" multiple
+              :options="formattedParticipants" id="participants">
               <template #leading>
-                <UIcon
-                  name="i-heroicons-user-group-20-solid"
-                  class="mr-2"
-                />
+                <UIcon name="i-heroicons-user-group-20-solid" class="mr-2" />
               </template>
               <template #empty>
                 None Selected
@@ -83,9 +58,12 @@
               <div v-for="(name, index) in meeting.externalParticipants" :key="index" class="relative">
                 <span class="bg-gray-200 rounded-md py-1 px-2 flex items-center">
                   {{ name }}
-                  <button type="button" @click="removeExternalParticipant(index)" class="ml-2 text-gray-400 hover:text-gray-600">
+                  <button type="button" @click="removeExternalParticipant(index)"
+                    class="ml-2 text-gray-400 hover:text-gray-600">
                     <svg class="h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414L11.414 10l2.293 2.293a1 1 0 11-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 010-1.414z" clip-rule="evenodd" />
+                      <path fill-rule="evenodd"
+                        d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 111.414 1.414L11.414 10l2.293 2.293a1 1 0 11-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 01-1.414-1.414L8.586 10 6.293 7.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd" />
                     </svg>
                   </button>
                 </span>
@@ -166,31 +144,51 @@ const isDateWithinNextMonth = (dateStr: string): boolean => {
   return isAfterToday && isBeforeNextMonth;
 };
 
+// const meetingForm = z.object({
+//   title: z.string().min(1, 'Title is required'),
+//   room: z.string().min(1, 'Room is required'),
+//   date: z.string()
+//     .refine(isValidDate, { message: 'Date is Required' })
+//     .refine(isDateWithinNextMonth, { message: 'Date must be within the next month and not in the past' }),
+//   start_time: z.string().refine((startTime: string) => {
+//     console.log("Time:", startTime)
+//     const startTimeDate = parse(startTime,'HH:MM',new Date())
+//     return isValid(startTimeDate)
+//   }, { message: 'Start time is required' }),
+//   end_time : z.string().refine((endTime:string) => {
+//     const endTimeDate = parse(endTime,'hh:mm ',new Date())
+//    ;
+//   }, {message : 'End time is required'}),
+//   formattedParticipants: z.array(z.string().min(1)).optional(),
+//   externalParticipants: z.array(z.string().min(1)).optional(),
+// }).refine((data) => {
+//   const startTimeDate = parse(data.start_time,'hh:mm a',new Date())
+//   console.log(startTimeDate)
+//   const endTimeDate = parse(data.end_time,'hh:mm a', new Date())
+//   const endTimePlus10Min = addMinutes(startTimeDate,10)
+//   return endTimeDate >= endTimePlus10Min
+// })
+
+const startOfDayToday = startOfDay(new Date())
+const startOfDayInAMonth = addMonths(startOfDayToday, 1)
+
 const meetingForm = z.object({
   title: z.string().min(1, 'Title is required'),
   room: z.string().min(1, 'Room is required'),
   date: z.string()
-    .refine(isValidDate, { message: 'Invalid date format' })
-    .refine(isDateWithinNextMonth, { message: 'Date must be within the next month and not in the past' }),
-  start_time: z.string().refine((startTime: string) => {
-    const startTimeDate = parse(startTime, 'h:mm a', new Date());
-    return !isNaN(startTimeDate.getTime());
-  }, { message: 'Invalid start time format' }),
-  end_time : z.string(),
-  // end_time: z.string().refine((endTime: string, data: { start_time: string }) => {
-  //   const startTimeDate = parse(data.start_time, 'h:mm a', new Date());
-  //   const endTimeDate = parse(endTime, 'h:mm a', new Date());
-  //   const endTimePlus10Min = addMinutes(startTimeDate, 10);
-  //   return endTimeDate >= endTimePlus10Min;
-  // }, { message: 'End time must be at least 10 minutes after start time' }),
+    .date()
+    .transform(str => parseDate(str))
+    .pipe(z.date().min(startOfDayToday, 'Start Date cannot be in the past').max(startOfDayInAMonth, 'Cannot schedule meetings more than a month in advance')),
+  start_time: z.string().refine(val => isValid(parse(val, 'HH:mm', new Date())), 'Start Time is Required'),
+  end_time: z.string().refine(val => isValid(parse(val, 'HH:mm', new Date())), 'End Time is required'),
   formattedParticipants: z.array(z.string().min(1)).optional(),
   externalParticipants: z.array(z.string().min(1)).optional(),
-});
+})
+
 const newExternalParticipant = ref('');
 const isOpen = ref(false);
 const addExternalParticipants = ref(false);
 
-const externalParticipantEmails = ref('');
 
 interface Participants {
   first_name: string;
@@ -208,6 +206,7 @@ watchEffect(() => {
 });
 
 const onSubmit = () => {
+  console.log(meeting.value.end_time)
   const validateForm = meetingForm.safeParse(meeting.value);
   if (!validateForm.success) {
     errors.value = validateForm.error.errors.map((err) => err.message);
@@ -240,4 +239,3 @@ const removeExternalParticipant = (index: number) => {
 };
 
 </script>
-
