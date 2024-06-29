a7cdcb9f0e2e6f48b886ab82f52331a257443b65
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-29 13:27:17 +0300

    chore : store email inside it

--- frontend/store/authStore.ts
+++ frontend/store/authStore.ts
@@ -3,6 +3,7 @@ import { jwtDecode } from "jwt-decode";
 const authStore = defineStore("auth", () => {
   const cookieToken = useCookie("auth-token");
   const user_id = ref<number>(-1);
+  const user_email = ref<string>("");
   const isAuthenticated = computed(() => {
     if (!cookieToken.value) return false;
     const decodedToken = jwtDecode(cookieToken.value as string);
@@ -17,23 +18,25 @@ const authStore = defineStore("auth", () => {
     return true
   });
 
-  const login = (newToken: string,id:number) => {
-    if(cookieToken.value) {
+  const login = (newToken: string, id: number, email: string) => {
+    if (cookieToken.value) {
       cookieToken.value = null
     }
     user_id.value = id
+    user_email.value = email
     cookieToken.value = newToken;
   };
   const logout = () => {
     cookieToken.value = null;
     user_id.value = -1
-
+    user_email.value = ""
   };
   return {
     login,
     logout,
     isAuthenticated,
-    user_id
+    user_id,
+    user_email
   };
 });
 
