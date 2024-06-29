6fecce88bd88f0bcab80cbd25158c36f5702d7a3
Jeremi-code <ermiassintayehu13@gmail.com>
2024-06-29 12:25:42 +0300

    fix : change image src

--- frontend/pages/rooms/index.vue
+++ frontend/pages/rooms/index.vue
@@ -10,7 +10,7 @@
               <span class="sr-only">View meeting room</span>
             </a> -->
             <img
-              src="https://www.airbnb.com/rooms/18155163?adults=1&category_tag=Tag%3A8678&children=0&enable_m3_private_room=true&infants=0&pets=0&photo_id=357232215&search_mode=flex_destinations_search&check_in=2024-07-13&check_out=2024-07-18&source_impression_id=p3_1718232511_P3RMjFWT39BVdZgL&previous_page_section_name=1000&federated_search_id=c0dde8b9-d7d6-4f4f-97d3-bf58b49e52de"
+              src="../../public/boardroom.jpg"
               width="400" height="300" alt="Meeting room" style="aspect-ratio: 400 / 300; object-fit: cover"
               class="w-full h-[200px] object-cover group-hover:opacity-80 transition-opacity" />
             <div class="p-4 bg-white dark:bg-gray-900">
@@ -31,7 +31,6 @@
                 <NuxtLink :to="{ name: 'rooms-id', params: { id: room.room_id } }"
                   class="inline-flex items-center justify-end pr-4 whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 rounded-md px-3 flex-1">
                   View Details
-
                 </NuxtLink>
               </div>
             </div>
