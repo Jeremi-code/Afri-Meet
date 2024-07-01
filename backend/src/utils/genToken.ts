7a63efd36b71c1afa6dd6fde13a7799c14d77a12
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-01 11:53:12 +0300

    chore : sign email and user id using the private key DES3

--- backend/src/utils/genToken.ts
+++ backend/src/utils/genToken.ts
@@ -1,12 +1,17 @@
 import jwt from 'jsonwebtoken'
 import { env } from '../env'
 
-const PRIVATE_KEY = env.PRIVATE_KEY
+const PRIVATE_KEY = env.PRIVATE_KEY 
 const PASSPHRASE = env.PASSPHRASE
 
-const genToken = (user_id: Number): string => {
-    const token = jwt.sign({ user_id }, { key: PRIVATE_KEY, passphrase: PASSPHRASE }, { expiresIn: '1h' })
-    return token
+const genToken = (user_id: Number, email: string): string => {
+    try {
+        const payload = { user_id, email }
+        const token = jwt.sign(payload, { key: PRIVATE_KEY, passphrase: PASSPHRASE }, {algorithm:"RS256", expiresIn: '1h' })
+        return token
+    } catch(error) {
+        throw new Error('Something Unknown happened')    
+    }
 }
 
 export default genToken
