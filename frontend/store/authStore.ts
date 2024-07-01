0990ca3b9abc3fcd915985fe4ba9890250e569bc
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-01 17:11:09 +0300

    fix : fix all problems related to auth state

--- frontend/store/authStore.ts
+++ frontend/store/authStore.ts
@@ -1,43 +1,39 @@
-import { defineStore } from "pinia";
-import { jwtDecode } from "jwt-decode";
-const authStore = defineStore("auth", () => {
-  const cookieToken = useCookie("auth-token");
-  const user_id = ref<number>(-1);
-  const user_email = ref<string>("");
+import { defineStore } from 'pinia';
+import { computed } from 'vue';
+
+
+export const useAuthStore = defineStore('authStore', () => {
+  const authCookie = useCookie<{ token: string, payload: { id: number, email: string } } | null>('auth-token');
+  const { onLogout } = useApollo()
+
+  const payload = computed(() => {
+    return authCookie.value?.payload ?? null
+  })
+  const user_id = computed(() => {
+    return payload.value?.id
+  })
+  const user_email = computed(() => {
+    return payload.value?.email
+  })
   const isAuthenticated = computed(() => {
-    if (!cookieToken.value) return false;
-    const decodedToken = jwtDecode(cookieToken.value as string);
-    const currentTime: number = Math.floor(Date.now() / 1000);
-    const expiredAt = decodedToken.exp;
-    if (expiredAt != undefined) {
-      if (expiredAt < currentTime) {
-        cookieToken.value = null;
-        return false
-      }
-    }
-    return true
+    return payload.value !== null
   });
 
   const login = (newToken: string, id: number, email: string) => {
-    if (cookieToken.value) {
-      cookieToken.value = null
-    }
-    user_id.value = id
-    user_email.value = email
-    cookieToken.value = newToken;
+    authCookie.value = { token: newToken, payload: { id, email } };
   };
+
   const logout = () => {
-    cookieToken.value = null;
-    user_id.value = -1
-    user_email.value = ""
+    authCookie.value = null;
+    onLogout()
   };
+
   return {
     login,
     logout,
+    payload,
     isAuthenticated,
     user_id,
-    user_email
+    user_email,
   };
 });
-
-export default authStore;
