f10597a4f189da5898abe302d1ea00281462788f
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-01 17:15:55 +0300

    chore : use a new name of auth store

--- frontend/middleware/auth.ts
+++ frontend/middleware/auth.ts
@@ -1,10 +1,9 @@
 import { defineNuxtRouteMiddleware } from "#app";
-import authStore from "~/store/authStore";
 import { useRouter } from "vue-router";
 
 
 export default defineNuxtRouteMiddleware ((to,from) => {
-    const store = authStore()
+    const store = useAuthStore()
     const router = useRouter()
     console.log(store.isAuthenticated)
     if(!store.isAuthenticated) {
