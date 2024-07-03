31557504861e2c18bbf15e4be40487c40c97875c
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-03 13:01:42 +0300

    refactor : define a function that get the current military time for different components

---
+++ frontend/utils/GetCurrentMilitaryTime.ts
@@ -0,0 +1,6 @@
+export  function getCurrentMilitaryTime(): string {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    return `${hours}:${minutes}`;
+  }
