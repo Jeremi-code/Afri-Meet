96263ae57e499d42ffe8dbbc2cb28a7361afc4bf
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-05 16:17:52 +0300

    refactor : refactor a composable to return a function

--- frontend/composables/customToast.ts
+++ frontend/composables/customToast.ts
@@ -1,24 +1,27 @@
 import { useToast } from "#imports";
-export function useCustomToast(message: string, status: string) {
-    const toast = useToast()
-    if (status == "ok") {
-        toast.add({
-            title: message,
-            color: 'green',
-            icon: 'i-heroicons-check-circle',
-            ui: {
-                backgroundColor: 'green'
+export function useCustomToast() {
+    const toast = useToast();
+    return {
+        add(message: string, status: string) {
+            if (status == "ok") {
+                toast.add({
+                    title: message,
+                    color: "green",
+                    icon: "i-heroicons-check-circle",
+                    ui: {
+                        backgroundColor: "green",
+                    },
+                });
+            } else {
+                toast.add({
+                    title: message,
+                    color: "red",
+                    icon: "i-heroicons-x-mark",
+                    ui: {
+                        backgroundColor: "bg-red-100",
+                    },
+                });
             }
-        })
-    } else {
-        toast.add({
-            title: message,
-            color: 'red',
-            icon: "i-heroicons-x-mark",
-            ui: {
-                backgroundColor: "bg-red-100"
-
-            }
-        });
-    }
+        },
+    };
 }
