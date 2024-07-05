d94d6865bca55f108e8f7b894dc2769ae2e9771a
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-05 16:16:08 +0300

    chore : config websocket endpoint

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -12,6 +12,7 @@ export default defineNuxtConfig({
     clients: {
       default: {
         httpEndpoint: "http://localhost:8080/v1/graphql",
+        wsEndpoint: 'ws://localhost:8080/v1/graphql',
       },
     },
   },
