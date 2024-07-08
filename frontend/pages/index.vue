d676536b7be09be7dc044fb7d36a1cea28f97c15
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-08 14:10:59 +0300

    add padding to the button

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -11,7 +11,7 @@ import TypeEffect from '~/components/TypeEffect.vue';
             <h1 class=" text-4xl font-bold mb-2">Making your meeting schedules a breathe.</h1>
             <p class="text-lg">Reserve venues and invite participants to your meetings.</p>
         <UContainer class="mt-6">
-            <UButton color="primary" class="mr-4" to="/meetings">Schedule Meeting</UButton>
+            <UButton color="primary" class="mr-4 p-3" to="/meetings">Schedule Meeting</UButton>
         </UContainer>
         </div>
     </div>
