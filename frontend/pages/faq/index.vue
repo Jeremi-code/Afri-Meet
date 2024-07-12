167b10a401c6a40243ba33e9481a170ee6c7721b
Wendi-code <wendimusit@gmai.com>
2024-07-12 11:10:30 +0300

    feat: develop first faq page

--- frontend/pages/faq/index.vue
+++ frontend/pages/faq/index.vue
@@ -1,3 +1,24 @@
 <template>
-    <h1>FAQ</h1>
+    <div class="mx-auto max-w-4xl py-12 md:py-16 lg:py-20 px-4 sm:px-6 lg:px-8">
+        <div class="space-y-8">
+            <div class="text-center">
+                <h1 class="text-4xl font-bold tracking-tight text-foreground sm:text-5xl">Frequently Asked Questions
+                </h1>
+                <p class="mt-4 text-lg text-muted-foreground">Get answers to the most common questions about our
+                    platform.</p>
+            </div>
+        </div>
+        <div class="space-y-6 mt-6">
+            <UAccordion open-icon="i-heroicons-plus" close-icon="i-heroicons-minus" :items="items" />
+        </div>
+    </div>
 </template>
+
+<script setup lang="ts">
+const items = [
+    { label: '1. What is Afri Meet?', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
+    { label: '2. Getting Started', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
+    { label: '3. Scheduling Meetings', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' },
+    { label: '4. Notifiactions', content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit' }
+];
+</script>
