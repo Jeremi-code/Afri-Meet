70877c8f01aeacd12f97113b9d52304108fbfd88
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-12 12:23:20 +0300

    remove faq link

--- frontend/components/NavBar.vue
+++ frontend/components/NavBar.vue
@@ -4,7 +4,7 @@
       <ULink class="text-lg font-bold text-gray-600 hover:text-gray transition-colors duration-300" to="/">
         Afrimeet
       </ULink>
-      <div class="hidden md:flex space-x-4 items-center">
+      <ul class="hidden md:flex space-x-4 items-center list-none">
         <li class="flex items-center">
           <BaseNavLink label="Meetings" to="/meetings" />
         </li>
@@ -14,9 +14,7 @@
         <li class="flex items-center">
           <BaseNavLink label="About us" to="/about" />
         </li>
-        <li class="flex items-center">
-          <BaseNavLink label="FAQ" to="/faq" />
-        </li>
+        
         <div v-if="!isAuthenticated" class="flex flex-row px-2">
           <li>
             <UButton class="border-50" to="/register">
@@ -36,7 +34,7 @@
             </UButton>
           </li>
         </div>
-      </div>
+      </ul>
       <button @click="toggleMobileMenu" class="md:hidden focus:outline-none">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
@@ -44,13 +42,14 @@
       </button>
     </div>
     <transition name="slide-right">
-      <div v-if="isMobileMenuOpen" class="fixed top-0 right-0 w-1/3 h-screen bg-white flex flex-col z-40 items-center transition-transform duration-300 ease-in-out">
+      <div v-if="isMobileMenuOpen" class="fixed top-0 right-0 mlsm:w-screen w-1/3 msm:w-1/2 lsm:w-2/3  h-screen bg-white flex flex-col z-40 transition-transform duration-300 ease-in-out">
+        
         <button @click="toggleMobileMenu" class="self-end focus:outline-none p-2">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
-        <ul class="mt-[160px] space-y-4 flex-grow">
+        <ul class="mt-[160px] space-y-4 flex-grow flex flex-col items-center">
           <li @click="toggleMobileMenu">
             <BaseNavLink label="Meetings" to="/meetings" />
           </li>
@@ -60,10 +59,8 @@
           <li @click="toggleMobileMenu">
             <BaseNavLink label="About us" to="/about" />
           </li>
-          <li @click="toggleMobileMenu">
-            <BaseNavLink label="FAQ" to="/faq" />
-          </li>
-          <div v-if="!isAuthenticated" class="flex flex-col space-y-4">
+          
+          <div v-if="!isAuthenticated" class="flex flex-row space-x-4 my-6">
             <li>
               <UButton class="border-50" to="/register">
                 Register
