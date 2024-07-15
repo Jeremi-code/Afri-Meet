a371e25e6f4510bd0f557f7aceadfa271ea61a9d
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-15 10:10:31 +0300

    remove console.log statement

--- frontend/pages/meetings/index.vue
+++ frontend/pages/meetings/index.vue
@@ -205,6 +205,7 @@ const joinedMeetingsList = computed<Meeting[]>(() => {
   return meetingData.value?.joinedMeetings.filter((meeting: any) => {
     return !createdMeetingIds.value.includes(meeting.meeting_id)
   }).map((meeting: any) => {
+    console.log(meeting)
     let room = roomsList.value.find((room) => room.room_id === meeting.room_id)
     return ({
       ...meeting,
