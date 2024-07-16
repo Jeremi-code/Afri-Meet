6378d65bb5cb48745e3279a4a2a961c8034177fa
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-16 14:41:52 +0300

    feat : show the title of the meeting that is going to be rescheduled

--- frontend/components/RescheduleModal.vue
+++ frontend/components/RescheduleModal.vue
@@ -11,7 +11,7 @@
         <template #header>
           <div class="flex items-center justify-between">
             <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
-              Reserve {{}}
+              Reschedule {{props.title}}
             </h3>
             <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
               @click="isOpen = false" />
