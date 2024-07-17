fbc15ee336fef1f86d11deb2c782cb32308d10f9
Jeremi-code <ermiassintayehu13@gmail.com>
2024-07-17 17:09:48 +0300

    feat : check for the expiration status of the token and navigate to login page if it is expired

--- frontend/store/authStore.ts
+++ frontend/store/authStore.ts
@@ -3,8 +3,17 @@ import { computed } from 'vue';
 
 
 export const useAuthStore = defineStore('authStore', () => {
-  const authCookie = useCookie<{ token: string, payload: { id: number, email: string } } | null>('auth-token');
+  const authCookie = useCookie<{ token: string, payload: { id: number, email: string, expAt: number } } | null>('auth-token');
   const { onLogout } = useApollo()
+  const currentTime = ref(Math.floor(Date.now() / 1000));
+  onMounted(() => {
+    const interval = setInterval(() => {
+      currentTime.value = Math.floor(Date.now() / 1000);
+    }, 1000);
+    onUnmounted(() => {
+      clearInterval(interval);
+    });
+  });
 
   const payload = computed(() => {
     return authCookie.value?.payload ?? null
@@ -16,17 +25,25 @@ export const useAuthStore = defineStore('authStore', () => {
     return payload.value?.email
   })
   const isAuthenticated = computed(() => {
-    return payload.value !== null
+    return payload.value?.expAt! > currentTime.value
   });
 
-  const login = (newToken: string, id: number, email: string) => {
-    authCookie.value = { token: newToken, payload: { id, email } };
+  const login = (newToken: string, id: number, email: string, expAt: number) => {
+    authCookie.value = { token: newToken, payload: { id, email, expAt } };
   };
 
   const logout = () => {
     authCookie.value = null;
     onLogout()
   };
+
+  watch(isAuthenticated, (newValue) => {
+    console.log(isAuthenticated)
+    if (!newValue) {
+      logout()
+      navigateTo('/login')
+    }
+  });
 
   return {
     login,
